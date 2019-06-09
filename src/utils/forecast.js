f7a8e6445d6a0f54f44a5d303e60f3b7009d5194
Bo <deleon.reescano@ricealumni.net>
2019-06-08 19:01:41 -0500

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,31 @@
+const request=require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url='https://api.darksky.net/forecast/a2569f43043975a58c7f8fd524710c6a/' + latitude + ',' + longitude
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather services!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degrees out.' + 'There is a ' + body.currently.precipProbability + ' chance of rain.')
+        }
+        
+    })
+}
+
+//const url = 'https://api.darksky.net/forecast/a2569f43043975a58c7f8fd524710c6a/37.8267,-122.4233'
+
+// request({ url: url, json:true}, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather services!')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location')
+//     } else {
+//         console.log('It is currently ' + response.body.currently.temperature + ' degrees out.')
+//         console.log('There is a ' + response.body.currently.precipProbability + ' chance of rain.')
+//         console.log(response.body.daily.data[0].summary)
+//     }
+// })
+
+module.exports = forecast
