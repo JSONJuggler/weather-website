75431ba31440692ec49c7971bbb86d48795a828a
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-07-02 07:36:18 -0500

    cant get fetch to fetch the actual url in the event listenr, so app is not working on portfolio domain, but i will revisit this app so this is ok for now

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,28 @@
-const weatherForm = document.querySelector('form')
-const searchInput = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const weatherForm = document.querySelector("form");
+const searchInput = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
-weatherForm.addEventListener('submit', (e) => {
-    // prevents page from refreshing after pressing search
-    e.preventDefault()
+weatherForm.addEventListener("submit", (e) => {
+  // prevents page from refreshing after pressing search
+  e.preventDefault();
 
-    const location = searchInput.value
-    const url = '/weather?address=' + location
+  const location = searchInput.value;
+  const url = "/weather?address=" + location;
 
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
 
-    fetch(url).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
-                messageTwo.textContent = ''
-            } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
-            }
-        })
-    })
-})
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = "";
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
+});
+
