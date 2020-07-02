5690e5259838b12c5987afe3cf783cc7d78abf1d
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-07-02 06:05:58 -0500

    prep for deployment on same domain as portfolio, hardcoded urls are only temporary until i revisit this app, will give issues in local dev environment ofc

--- src/app.js
+++ src/app.js
@@ -1,112 +1,111 @@
-const express = require('express')
-const path = require('path')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Beau Reescano'
-    })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Beau Reescano",
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Beau Reescano'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Beau Reescano",
+  });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Help',
-        helpMsg: 'Welcome to the blank help page!',
-        name: 'Beau Reescano'
-    })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    helpMsg: "Welcome to the blank help page!",
+    name: "Beau Reescano",
+  });
+});
 
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address'
-        })
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+
+  const addressInput = req.query.address;
+
+  geocode(addressInput, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({
+        error,
+      });
     }
-    
-    const addressInput = req.query.address
-
-    geocode(addressInput, (error, { latitude, longitude, location} = {}) => {
-        if (error) {
-            return res.send({
-                error
-            })
-        }
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({
-                    error
-                })
-            }
-            res.send({
-                forecast: forecastData,
-                address: req.query.address,
-                location,
-                latitude,
-                longitude
-            })
-          })
-    })
-})
-
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
         return res.send({
-            error: 'You must provide a search term'
-        })
-    }
+          error,
+        });
+      }
+      res.send({
+        forecast: forecastData,
+        address: req.query.address,
+        location,
+        latitude,
+        longitude,
+      });
+    });
+  });
+});
 
-    res.send({
-        products: []
-    })
-})
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
 
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        msg: 'Help article not found',
-        title: 'Help 404',
-        name: 'Beau Reescano'
+  res.send({
+    products: [],
+  });
+});
 
-    })
-})
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    msg: "Help article not found",
+    title: "Help 404",
+    name: "Beau Reescano",
+  });
+});
 
-app.get('*', (req, res) => {
-    res.render('404', {
-        msg: 'Page not found',
-        title: '404',
-        name: 'Beau Reescano'
-    })
-})
+app.get("*", (req, res) => {
+  res.render("404", {
+    msg: "Page not found",
+    title: "404",
+    name: "Beau Reescano",
+  });
+});
 
 // app.com
 // app.com/help
 // app.com/about
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+  console.log("Server is up on port " + port);
+});
